b18ff1fcbff262cf733b1de0063429b3fccd2549
jungwon <mac2001035@gmail.com>
2024-07-22 18:19:41 +0900

    CHORE: Lombok 추가.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
+
 public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
