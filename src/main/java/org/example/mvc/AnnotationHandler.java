72589fd059fb518d5610b5ebad94b77ee36b5598
jungwon <mac2001035@gmail.com>
2024-08-05 00:34:06 +0900

    FEAT: AnnotationHandler 구현체 구현.

--- src/main/java/org/example/mvc/AnnotationHandler.java
+++ src/main/java/org/example/mvc/AnnotationHandler.java
@@ -1,9 +1,23 @@
 package org.example.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class AnnotationHandler {
-    public AnnotationHandler(Class<?> clazz, Method declaredMethod) {
+    private final Class<?> clazz;
+    private final Method targetMethod;
 
+    public AnnotationHandler(Class<?> clazz, Method targetMethod) {
+        this.clazz = clazz;
+        this.targetMethod = targetMethod;
+    }
+
+    public String handle(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Constructor<?> declaredConstructor = clazz.getDeclaredConstructor();
+        Object handler = declaredConstructor.newInstance();
+
+        return (String) targetMethod.invoke(handler,request,response);
     }
 }
